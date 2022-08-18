d8c1a2cf3d8a4b2e3871072f236e94b430d4812a
MAKS <5059585@mail.ru>
2022-08-18 16:28:13 +0300

    Counter 18.08.22 work version

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,29 @@
 import React, {useState} from 'react';
-import {Count} from './component/Count';
-import {Set} from './component/Set';
+import {Count} from './Count';
+import {Set} from './Set';
 import './App.css'
 
 export const App = () => {
-    let [maxValue, setMaxValue] = useState(1);//set max count
-    let [minValue, setMinValue] = useState(0);// set min count
-    let [value, setValue] = useState<number>(minValue); // счетчик
-    let [mistake, setMistake] = useState<string>(String(minValue)); //сет ошибки
-    let [minValueSet, setMinValueSet] = useState<number>(minValue)//забираю значение из инпута
-    let [maxValueSet, setMaxValueSet] = useState<number>(maxValue)//забираю значение из инпута
+    let [maxValue, setMaxValue] = useState(1);
+    let [minValue, setMinValue] = useState(0);
+    let [value, setValue] = useState<number>(minValue);
+    let [mistake, setMistake] = useState<string>(String(minValue));
 
-    const setMinValueSetSet = (value: string) => {
-        setMinValueSet(Number(value))
-    }
-    // ловим значение инпута
-    const setMaxValueSetSet = (value: string) => {
-        setMaxValueSet(Number(value))
-    }
-    // ловим значение инпута
-    const disableButton = () => {
-        if (((minValueSet === minValue) && (maxValueSet === maxValue))
-            || (incorrectValue())) {
-            return true
-        }
-    }
-    // дизейбл кнопки
-
-    const setButton = () => {
-        setMaxValue(maxValueSet) // сетаем мах значение в начальный стейт
-        setMinValue(minValueSet)// сетаем мин значение в начальный стейт
-        setMistake(String(value)) // передача значений в импут счетчика
-        setValue(minValueSet)
+    const setButton = (maxValueSet: number, minValueSet: number) => {
+        setMaxValue(maxValueSet)
+        setMinValue(minValueSet)
+        setMistake(String(value))
+        localStorage.setItem('maxValueStart', JSON.stringify(maxValueSet))
+        localStorage.setItem('minValueStart', JSON.stringify(minValueSet))
         return (
-            setValue(minValueSet)
+            (setValue(minValue))
+
         )
     }
     const reset = () => {
-        setMistake(String(minValueSet))
+        setMistake(String(minValue))
         return (
-            setValue(minValueSet)
+            setValue(minValue)
         )
     }
     const inc = () => {
@@ -48,28 +32,29 @@ export const App = () => {
         }
         setMistake(String(value))
         return setValue(value)
-    } // если значение счетчика меньше махимального то передаем значение в стэйт ошибки и возращаем это значение
-    const incorrectValue = () => {
-        if (minValueSet < 0) {
+    }
+    const incorrectValue = (maxValue: number, minValue: number) => {
+
+        if (minValue < 0) {
             setMistake('Incorrect value')
-        } else if (maxValueSet <= minValueSet) {
+        } else if (maxValue <= minValue) {
             setMistake('Incorrect value')
         } else {
-            setValue(minValueSet)
+            setValue(minValue)
             setMistake('enter values and press \'set\'')
         }
-        return ((maxValueSet <= minValueSet) || (minValueSet < 0))
+
+        return ((maxValue <= minValue) || (minValue < 0))
+
     }
     return (
         <div className="App1">
             <div className="App">
                 <Set
+                    maxValue={maxValue}
+                    minValue={minValue}
                     setButton={setButton}
-                    setMinValueSetSet={setMinValueSetSet}
-                    minValueSet={minValueSet}
-                    setMaxValueSetSet={setMaxValueSetSet}
-                    maxValueSet={maxValueSet}
-                    disableButton={disableButton}
+                    incorrectValue={incorrectValue}
                 />
             </div>
             <div className="App">
